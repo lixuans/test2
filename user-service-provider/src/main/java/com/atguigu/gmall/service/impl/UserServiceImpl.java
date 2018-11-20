e1d9eb43573d987681180423f7658a765d4d04eb
lixuan <869110009@qq.com>
2018-11-20 16:55:51 +0800

    测试Git分支1

--- user-service-provider/src/main/java/com/atguigu/gmall/service/impl/UserServiceImpl.java
+++ user-service-provider/src/main/java/com/atguigu/gmall/service/impl/UserServiceImpl.java
@@ -12,7 +12,7 @@ public class UserServiceImpl implements UserService {
 	public List<UserAddress> getUserAddressList(String userId) {
 		System.out.println("UserServiceImpl.....old...");
 		// TODO Auto-generated method stub
-		UserAddress address1 = new UserAddress(1, "同程1", "1", "李宣", "00000000000", "Y");
+		UserAss3213123123 address11232312312312312 = new UserAddress(1, "同程1", "1", "李宣", "00000000000", "Y");
 		UserAddress address2 = new UserAddress(2, "同程2", "1", "王宣", "00000000000", "N");
 		/*try {
 			Thread.sleep(4000);

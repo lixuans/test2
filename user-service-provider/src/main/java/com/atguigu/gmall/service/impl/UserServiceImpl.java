afa0f39df86db187a823e070affc1e04804406c8
lixuan <869110009@qq.com>
2018-11-20 16:35:54 +0800

    测试Git

---
+++ user-service-provider/src/main/java/com/atguigu/gmall/service/impl/UserServiceImpl.java
@@ -0,0 +1,26 @@
+package com.atguigu.gmall.service.impl;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.atguigu.gmall.bean.UserAddress;
+import com.atguigu.gmall.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+
+	public List<UserAddress> getUserAddressList(String userId) {
+		System.out.println("UserServiceImpl.....old...");
+		// TODO Auto-generated method stub
+		UserAddress address1 = new UserAddress(1, "同程艺龙1", "1", "李宣", "00000000000", "Y");
+		UserAddress address2 = new UserAddress(2, "同程艺龙2", "1", "王宣", "00000000000", "N");
+		/*try {
+			Thread.sleep(4000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		return Arrays.asList(address1,address2);
+	}
+
+}
